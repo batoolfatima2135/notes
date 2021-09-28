2fa7382300fac6fe17f749e02e6204a9791b6dd2
Batool Fatima <batoolfatima2135@gmail.com>
2021-09-29 00:18:14 +0500

    more Changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\SendEmail;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\mail\SendMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+
 
 class RegisterController extends Controller
 {
@@ -64,10 +68,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+
+       $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        Mail::to($data['email'])->send(new SendEmail($user));
+
+        return $user;
     }
 }
---
+++ resources/views/mail.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Email</title>
+</head>
+<body>
+    <h3>Hey {{$user->name}}</h3>
+    <p>Thankyou so much for registering on our website.
+        <br>
+        We will try our best to satisfy you!
+    </p>
+</body>
+</html>
